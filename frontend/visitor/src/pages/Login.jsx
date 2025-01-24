5a733463758b9be3bdc4d34b2e0bc09a4b964716
mathusrivijayakumar <143775107+mathusrivijayakumar@users.noreply.github.com>
2025-01-24 19:17:19 +0530

    Update Login.jsx

--- frontend/visitor/src/pages/Login.jsx
+++ frontend/visitor/src/pages/Login.jsx
@@ -78,12 +78,7 @@ const Login = () => {
             >
               Register
             </button>
-            <button
-              onClick={() => setShowForgotPassword(true)}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Forgot Password
-            </button>
+            
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
